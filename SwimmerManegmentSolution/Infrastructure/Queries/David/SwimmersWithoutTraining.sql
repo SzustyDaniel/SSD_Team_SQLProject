61527b6f1d53f6b142076c1b34c48c63f5c92ef1
David Fineboym <kurbanov0409@gmail.com>
2019-02-02 13:55:45 +0200

    Added folders for queries.

---
+++ SwimmerManegmentSolution/Infrastructure/Queries/David/SwimmersWithoutTraining.sql
@@ -0,0 +1,12 @@
+﻿--Please review.
+
+SELECT rs.SwimmerID, 
+       s.FirstName, 
+       s.LastName, 
+       s.Gender, 
+       rs.Team, 
+       rs.TutorID
+FROM tblRegularSwimmer AS rs
+     LEFT OUTER JOIN tblSwimmerTraining AS st ON rs.SwimmerID = st.SwimmerID
+     INNER JOIN tblSwimmer AS s ON rs.SwimmerID = s.SwimmerID
+WHERE(st.SwimmerID IS NULL);
