be33f95bdf386c48292ddfd555f4160ab3038c54
David Fineboym <kurbanov0409@gmail.com>
2019-02-04 18:46:53 +0200

    Added ADO interaction.

--- SwimmerManegmentSolution/Infrastructure/SqlHelper.cs
+++ SwimmerManegmentSolution/Infrastructure/SqlHelper.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    public class SqlHelper
+    public static class SqlHelper
     {
+        public static List<T> GetAllRowsFromDb<T>(string connectionString, string query) where T : new()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                List<T> list = new List<T>();
+                PropertyInfo[] publicProperties = typeof(T).GetProperties();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                T row = new T();
+                while (reader.Read())
+                {
+                    foreach (PropertyInfo property in publicProperties)
+                    {
+                        object value = reader[property.Name];
+                        property.SetValue(row, value is DBNull ? null : value);
+                    }
+                }
+                list.Add(row);
+                return list;
+            }
+        }
     }
 }
