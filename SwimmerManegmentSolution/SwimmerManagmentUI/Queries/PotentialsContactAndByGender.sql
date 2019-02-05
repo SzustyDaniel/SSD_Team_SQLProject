72c0c2b7a9665ef7026f4a88b783f410ee8c5b47
Shay Musachanov <shaybarca@gmail.com>
2019-02-05 11:22:57 +0200

    add sql client helper class

---
+++ SwimmerManegmentSolution/SwimmerManagmentUI/Queries/PotentialsContactAndByGender.sql
@@ -0,0 +1,24 @@
+﻿--Please review.
+
+SELECT s.SwimmerID, 
+       MIN(s.FirstName) AS FirstName, 
+       MIN(s.LastName) AS LastName, 
+       MIN(DATEDIFF(YEAR, s.BirthDate, GETDATE())) AS Age, 
+       COUNT(*) AS NumberOfTrainings
+FROM tblPotentialSwimmer AS ps
+     INNER JOIN tblSwimmer AS s ON ps.PotentialSwimmerID = s.SwimmerID
+     INNER JOIN tblSwimmerTraining AS st ON ps.PotentialSwimmerID = st.SwimmerID
+WHERE(ps.DateOfFirstContact >= DATEADD(MONTH, @monthsToAdd, GETDATE()))
+GROUP BY s.SwimmerID
+UNION
+SELECT s.SwimmerID, 
+       MIN(s.FirstName) AS FirstName, 
+       MIN(s.LastName) AS LastName, 
+       MIN(DATEDIFF(YEAR, s.BirthDate, GETDATE())) AS Age, 
+       COUNT(*) AS NumberOfTrainings
+FROM tblRegularSwimmer AS rs
+     INNER JOIN tblSwimmer AS s ON rs.SwimmerID = s.SwimmerID
+     INNER JOIN tblSwimmerTraining AS st ON rs.SwimmerID = st.SwimmerID
+WHERE(rs.Team IS NOT NULL)
+     AND (LOWER(s.Gender) = LOWER(@gender))
+GROUP BY s.SwimmerID;
---
+++ SwimmerManegmentSolution/SwimmerManagmentUI/ViewModels/SqlClientHelper.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure;
+using Infrastructure.Models;
+using SwimmerManagmentUI.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwimmerManagmentUI.ViewModels
+{
+    public static class SqlClientHelper
+    {
+        private static readonly Dictionary<Type, string> typeToQuery = new Dictionary<Type, string>
+        {
+            {typeof(Coach),QueriesManager.GetAllCoaches},
+            {typeof(RegularSwimmer),QueriesManager.GetAllCoaches},
+            {typeof(StupidClass),QueriesManager.GetAllCoaches },
+            {typeof(Team),QueriesManager.GetAllCoaches }
+        };
+
+        public static async Task<List<T>> Get<T>() where T : new()
+        {
+            if (typeToQuery.TryGetValue(typeof(T), out string query))
+            {
+                var connectionString = SqlConnectionStringHelper.ConnectionStringInAppConfig;
+                return await SqlHelper.GetAllRowsFromDbAsync<T>(connectionString, query).ConfigureAwait(false);
+            }
+            else
+            {
+                throw new Exception("Query not found");
+            }
+        }
+    }
+}
